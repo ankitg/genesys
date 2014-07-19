07b4c694913fb19f2626566b14c51d64c4099546
Ankit Guglani <ankitg@gmail.com>
2014-07-18 20:54:06 -0400

    - Added Android Library project

---
+++ Android/src/com/genesys/chatlib/models/Chat.java
@@ -0,0 +1,75 @@
+package com.genesys.chatlib.models;
+
+public class Chat {
+	private String firstName;
+	private String lastName;
+	private String nickname;
+	private String subject;
+	private String email;
+	
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the nickname
+	 */
+	public String getNickname() {
+		return nickname;
+	}
+	/**
+	 * @param nickname the nickname to set
+	 */
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	 @Override
+	 public String toString() {
+	    return "chat about " + subject;
+	 }
+}
